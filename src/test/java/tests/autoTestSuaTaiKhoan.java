cdd2ec702a1da2bc63aaac811fd9860df04d8eb8
tuandangnhap <tuanphani.c.t@gmail.com>
2021-06-15 21:53:42 +0700

    ok

--- src/test/java/tests/autoTestSuaTaiKhoan.java
+++ src/test/java/tests/autoTestSuaTaiKhoan.java
@@ -1,5 +1,16 @@
 package tests;
 
+import pages.DeleteAccountPage;
+import pages.LoginPage;
+import pages.SignUpPage;
+import utilities.ReadExcel;
+
 public class autoTestSuaTaiKhoan {
+	String mang[];
 	
+	ReadExcel testcase = new ReadExcel();
+	SignUpPage signUpPg;
+	LoginPage loginPg;
+	DeleteAccountPage deleteAccountPg;
+
 }
